8192cee15b0fc88bd4b596c3db0917c41458530e
Daniel Karpierz <dankarp1999@gmail.com>
2022-08-05 14:55:32 -0400

    Change table appending method from innerHTML to innerText and add the ability to remove books

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ const bookThree = new Book('Book #3', 'J.D. Salinger', 295, true);
 
 let myLibrary = [bookOne, bookTwo, bookThree];
 
+// let myLibrary = [];
+
 const form = document.querySelector('form');
 const table = document.querySelector('table');
 
@@ -21,20 +23,31 @@ function addBookToLibrary(e) {
 	const author = document.querySelector('input[name="author"]').value;
 	const pages = document.querySelector('input[name="pages"]').value;
 	const read = document.querySelector('input[name="read"]');
+
+	let delButton = document.createElement('button');
+	delButton.type = 'button';
+	delButton.className = 'deleteBtn';
+	delButton.innerText = 'x';
+
 	if (read.checked) {
 		readStatus = 'Read';
 	} else {
 		readStatus = 'Not Read';
 	}
-	table.innerHTML += `
-    <tr>
-        <td>${title}</td>
-        <td>${author}</td>
-        <td>${pages}</td>
-        <td>${readStatus}</td>
-        <td><button class="deleteBtn">x</button></td>
-    <tr>
-    `;
+
+	let newRow = table.insertRow(table.rows.length);
+
+	let cell1 = newRow.insertCell(0);
+	let cell2 = newRow.insertCell(1);
+	let cell3 = newRow.insertCell(2);
+	let cell4 = newRow.insertCell(3);
+	let cell5 = newRow.insertCell(4);
+
+	cell1.innerText = title;
+	cell2.innerText = author;
+	cell3.innerText = pages;
+	cell4.innerText = readStatus;
+	cell5.appendChild(delButton);
 
 	const newBook = new Book(title, author, pages, readStatus);
 	myLibrary.push(newBook);
@@ -55,29 +68,50 @@ function displayBooks() {
 			read = 'Not Read';
 		}
 
-		table.innerHTML += `
-    <tr>
-        <td>${title}</td>
-        <td>${author}</td>
-        <td>${pages}</td>
-        <td>${read}</td>
-        <td><button class="deleteBtn">x</button></td>
-    <tr>
-    `;
+		let delButton = document.createElement('button');
+		delButton.type = 'button';
+		delButton.className = 'deleteBtn';
+		delButton.innerText = 'x';
+		delButton.dataset.bookNumber = i;
+		myLibrary[i].bookNumber = i;
+
+		let newRow = table.insertRow(table.rows.length);
+
+		let cell1 = newRow.insertCell(0);
+		let cell2 = newRow.insertCell(1);
+		let cell3 = newRow.insertCell(2);
+		let cell4 = newRow.insertCell(3);
+		let cell5 = newRow.insertCell(4);
+
+		cell1.innerText = title;
+		cell2.innerText = author;
+		cell3.innerText = pages;
+		cell4.innerText = read;
+		cell5.appendChild(delButton);
 	}
 }
-
-displayBooks();
 
 function deleteBook(e) {
 	if (!e.target.classList.contains('deleteBtn')) {
 		return;
 	}
-	console.log(e.target);
-
 	const btn = e.target;
-	btn.closest('tr').remove();
+
+	let myLibraryLength = myLibrary.length;
+
+	for (let i = 0; i < myLibraryLength; i++) {
+		if (btn.dataset.bookNumber == myLibrary[i].bookNumber) {
+			console.log(
+				`You just removed ${myLibrary[i].title} from your library`
+			);
+			btn.closest('tr').remove();
+			myLibrary.splice(i, 1);
+			return myLibrary;
+		}
+	}
 }
 
 form.addEventListener('submit', addBookToLibrary);
 table.addEventListener('click', deleteBook);
+
+displayBooks();
